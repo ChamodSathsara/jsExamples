80fb4e1365a64044c29b85fe36c8db5a3b5517ae
ChamodSathsara <sathsarawapc@gmail.com>
2024-04-18 09:52:10 +0530

    js variables and datatypes

---
+++ js/dataTypes.js
@@ -0,0 +1,78 @@
+// number
+
+let number1 = 10;
+let number2 = 20;
+
+let total = number1+number2;
+console.log(total);
+console.log(typeof total+" ");
+
+
+
+//brack
+console.log(" ");
+
+
+//String
+let name1 = "kamal";
+let name2 = "perera";
+
+let fullName = name1+" "+name2;
+console.log(fullName);
+console.log(typeof fullName);
+
+
+//brack
+console.log(" ");
+
+
+//boolean
+let isSave = true;
+
+console.log(isSave);
+console.log(typeof isSave);
+
+//brack
+console.log(" ");
+
+//undefined
+let kamal;
+let malki = undefined;
+
+console.log(kamal);
+console.log(malki);
+
+//brack
+console.log(" ");
+
+//null
+let jaga = null;
+
+console.log(jaga);
+
+//brack
+console.log(" ");
+
+//const variable keyword
+const surName = "kamantha";
+// surName = "suda"; //don't allows bcs that is final
+
+console.log(surName);
+
+
+//let most use variable keyword
+
+//var =>> thatis variable keyword too
+
+//main 3 keywords
+// 1.let
+// const
+// var
+
+
+// datatypes
+// number
+// String
+// boolean
+// null
+// undifined
